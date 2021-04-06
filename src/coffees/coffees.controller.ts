7ef68741c0c3ab04038f410a0c66c1c79d95575a
Noah <hanlun.song@gmail.com>
2021-04-06 14:51:43 -0400

    post body

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +10,10 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: number) {
     return `This action returns the coffee with id of ${id}.`;
+  }
+
+  @Post()
+  create(@Body('name') name: string) {
+    return name;
   }
 }
