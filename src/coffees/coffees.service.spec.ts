625bbe632a90b79a2007ff05cc93fada637e3251
Noah <hanlun.song@gmail.com>
2021-04-08 13:25:14 -0400

    add a coffee service test

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,14 +1,28 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { CoffeesService } from './coffees.service';
+import coffeesConfig from './config/coffees.config';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+        { provide: ConfigService, useValue: { get: () => ({}) } },
+        { provide: coffeesConfig.KEY, useValue: {} },
+      ],
     }).compile();
 
+    console.log(service);
     service = module.get<CoffeesService>(CoffeesService);
   });
 
